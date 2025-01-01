7423bdda2103e657021c45ed433a4d397f86359f
JosephOlu <161716305+JosephOluOlofinte@users.noreply.github.com>
2025-01-01 15:36:03 +0100

    update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 - Project name: Sleat
 - Desc: Online Course E-Learning Educational Website
-- UI by: Praha from [Figma Community] (https://www.figma.com/@praha)
+- UI by: Praha from [Figma Community](https://www.figma.com/@praha)
 - Frontend Technology: React + TypeScript + Vite
 - [GitHub Repository](https://github.com/JosephOluOlofinte/sleat.git) 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ export default {
   ],
   theme: {
     screens: {
-      xsm: '320px',
+      xs: '320px',
       sm: '480px',
       md: '768px',
       lg: '1024px',
@@ -18,11 +18,26 @@ export default {
         'bodyFont': ['Figtree'],
       },
       colors: {
-        'bgBlue': '#12283A',
-        'titleBlue': '#008BC2',
-        'buttonGreen': '#33A653',
-        'textBlue': '#9DD3FF',
-        'boxBG': '#01041D'
+        'orange50': '#FF9500',
+        'orange70': '#FFBF66',
+        'orange75': '#FFCA80',
+        'orange80': '#FFD499',
+        'orange90': '#FFEACC',
+        'orange95': '#FFF4E5',
+        'orange97': '#FFF9F0',
+        'orange99': '#FFFDFA',
+        'white90': '#E4E4E7',
+        'white95': '#F1F1F3',
+        'white97': '#F7F7F8',
+        'white99': '#FCFCFD',
+        'grey10': '#191919',
+        'grey15': '#262626',
+        'grey20': '#333333',
+        'grey30': '#4C4C4D',
+        'grey35': '#59595A',
+        'grey40': '#656567',
+        'grey60': '#98989A',
+        'grey70': '#B3B3B3',
       },
 
       backgroundImage: {
