f62fa0e28e56775aaa2c1cac8775770a45204230
JosephOlu <161716305+JosephOluOlofinte@users.noreply.github.com>
2025-01-04 12:05:28 +0100

    update

--- src/pages/home/Video.tsx
+++ src/pages/home/Video.tsx
@@ -2,7 +2,6 @@ import homeVideo from '../../assets/videos/home-vid.mp4'
 const Video = () => {
   return (
     <section>
-        Video
         <video src={homeVideo} width="750" height="500" controls></video>
     </section>
   )
