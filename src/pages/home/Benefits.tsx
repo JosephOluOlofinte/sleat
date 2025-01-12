97aa6a6edb48455b23b863e1b7935811efd41b57
JosephOlu <161716305+JosephOluOlofinte@users.noreply.github.com>
2025-01-12 22:42:54 +0100

    started the benefits section. Everything works great

--- src/pages/home/Benefits.tsx
+++ src/pages/home/Benefits.tsx
@@ -8,8 +8,17 @@ import { Fragment } from "react/jsx-runtime"
 const Benefits = () => {
   return (
     <Fragment>
-        <section className="h-[100vh] w-[100%] flex justify-center">
-            <div className="h-[100%] w-[90%]"></div>
+        <section className="min-h-[100vh] w-[100%] flex justify-center mt-[70px]">
+            <div className="h-[100%] w-[90%] grid">
+              <div className="grid">
+                <h2 className="font-semibold text-[32px] text-grey15">Benefits</h2>
+                <div className="flex justify-between items-baseline">
+                  <p className="max-w-[82ch]">Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam eget elit id imperdiet et. Cras eu sit dignissim lorem nibh et. Ac cum eget habitasse in velit fringilla feugiat senectus in.</p>
+                  <a href="">View all</a>
+                </div>
+              </div>
+              <div></div>
+            </div>
         </section>
     </Fragment>
   )
--- src/pages/home/Video.tsx
+++ src/pages/home/Video.tsx
@@ -1,5 +1,5 @@
 import { Fragment } from 'react/jsx-runtime'
-import homeVideo from '../../assets/videos/home-vid.mp4'
+// import homeVideo from '../../assets/videos/home-vid.mp4'
 const Video = () => {
   return (
     <Fragment>
@@ -9,11 +9,9 @@ const Video = () => {
       w-[100%]
       m-0
       '>
-        
-        <div className='
-        flex justify-center items-start
-        w-[90%] h-[650px]'>
-          {/* <video src={homeVideo} className='h-[720px] rounded-[12px] border-8 border-orange50'></video> */}
+
+        <div className="flex justify-center items-start w-[90%] h-[650px] bg-orange80">
+          
         </div>
       </section>
     </Fragment>
