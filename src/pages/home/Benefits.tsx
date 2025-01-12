c9dbb9c9edcaa5bd2b00b4b209cc8fdc48b68768
JosephOlu <161716305+JosephOluOlofinte@users.noreply.github.com>
2025-01-12 22:50:38 +0100

    Finished benefit header. Everything works fine

--- src/pages/home/Benefits.tsx
+++ src/pages/home/Benefits.tsx
@@ -12,9 +12,9 @@ const Benefits = () => {
             <div className="h-[100%] w-[90%] grid">
               <div className="grid">
                 <h2 className="font-semibold text-[32px] text-grey15">Benefits</h2>
-                <div className="flex justify-between items-baseline">
-                  <p className="max-w-[82ch]">Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam eget elit id imperdiet et. Cras eu sit dignissim lorem nibh et. Ac cum eget habitasse in velit fringilla feugiat senectus in.</p>
-                  <a href="">View all</a>
+                <div className="flex justify-between items-end line leading-6 ">
+                  <p className="max-w-[82ch] text-grey35 mt-[5px]">Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam eget elit id imperdiet et. Cras eu sit dignissim lorem nibh et. Ac cum eget habitasse in velit fringilla feugiat senectus in.</p>
+                  <a href="" className="bg-absoluteWhite text-absoluteBlack text-[14px] font-medium py-[12px] px-[20px]">View all</a>
                 </div>
               </div>
               <div></div>
