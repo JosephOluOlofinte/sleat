5a0b57ac8f43de7ea36d00bced2fb0094d545c39
JosephOlu <161716305+JosephOluOlofinte@users.noreply.github.com>
2025-01-18 15:50:22 +0100

    Finished BenefitsCard. everything works fine

--- src/components/block/BenefitsCard.tsx
+++ src/components/block/BenefitsCard.tsx
@@ -3,15 +3,14 @@
 // import React from 'react'
 
 interface BenefitsProps {
-  id: number;
+  id: string;
   title: string;
   desc: string;
   url: string;
-  arrow: string;
   };
-}
 
-const BenefitsCard: React.FC<BenefitsProps> = ({id, title, desc, url ="", arrow = "",}) => {
+
+const BenefitsCard: React.FC<BenefitsProps> = ({id, title, desc, url ="",}) => {
   return (
                 <div className="bg-absoluteWhite p-[30px] xltablet:p-[40px] desktop:p-[50px] grid gap-[30px] xltablet:gap-[40px] desktop:gap-[50px] rounded-[10px]">
 
@@ -24,7 +23,6 @@ const BenefitsCard: React.FC<BenefitsProps> = ({id, title, desc, url ="", arrow 
                   
                   <div className="flex justify-end">
                     <a href={url} className="bg-white99 p-[14px] desktop:p-[20px] border-[2px] rounded-[6px] h-fit">
-                      {arrow}
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="size-6 text-orange50">
                         <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
                       </svg>
--- src/pages/home/Benefits.tsx
+++ src/pages/home/Benefits.tsx
@@ -2,6 +2,7 @@
 
 import { Fragment } from "react/jsx-runtime"
 import Button from "../../components/block/Button"
+import BenefitsCard from "../../components/block/BenefitsCard"
 
 
 
@@ -20,49 +21,42 @@ const Benefits = () => {
               </div>
 
               <div className="grid grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-3 grid-rows-1 tablet:grid-rows-2 gap-[20px] mt-[60px]">
-                <div className="bg-absoluteWhite p-[30px] xltablet:p-[40px] desktop:p-[50px] grid gap-[30px] xltablet:gap-[40px] desktop:gap-[50px] rounded-[10px]">
 
-                  <h2 className="text-right font-bold text-[50px] xltablet:text-[60px] desktop:text-[80px] ">01</h2>
+                <BenefitsCard 
+                  id="01" 
+                  title="Flexible Learning Schedule" 
+                  desc="Fit your coursework around your existing commitments and obligations" 
+                  url="https://www.goodemailcode.com/email-code/template" />
 
-                  <div className="grid gap-[14px] xltablet:gap-[16px] desktop:gap-[20px]">
-                    <h3 className="text-[20px] desktop:text-[24px] font-semibold text-[#333333]">Flexible Learning Schedule</h3>
-                    <p className="text-grey30 xlmobile:text-[16px] desktop:text-[18px] leading-[1.5] ">Fit your coursework around your existing commitments and obligations</p>
-                  </div>
-                  
-                  <div className="flex justify-end">
-                    <a href="" className="bg-white99 p-[14px] desktop:p-[20px] border-[2px] rounded-[6px] h-fit">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="size-6 text-orange50">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-                      </svg>
-                    </a>
-                  </div>
-                </div>
+                <BenefitsCard 
+                  id="01" 
+                  title="Flexible Learning Schedule" 
+                  desc="Fit your coursework around your existing commitments and obligations" 
+                  url="https://www.goodemailcode.com/email-code/template" />
 
-                <div className="bg-absoluteWhite h-[370px]">
-                  <h1>02</h1>
-                  <div></div>
-                  <div></div>
-                </div>
-                <div className="bg-absoluteWhite h-[370px]">
-                  <h1>03</h1>
-                  <div></div>
-                  <div></div>
-                </div>
-                <div className="bg-absoluteWhite h-[370px]">
-                  <h1>04</h1>
-                  <div></div>
-                  <div></div>
-                </div>
-                <div className="bg-absoluteWhite h-[370px] hidden tablet:block ">
-                  <h1>05</h1>
-                  <div></div>
-                  <div></div>
-                </div>
-                <div className="bg-absoluteWhite h-[370px] hidden tablet:block ">
-                  <h1>06</h1>
-                  <div></div>
-                  <div></div>
-                </div>
+                <BenefitsCard 
+                  id="01" 
+                  title="Flexible Learning Schedule" 
+                  desc="Fit your coursework around your existing commitments and obligations" 
+                  url="https://www.goodemailcode.com/email-code/template" />
+
+                <BenefitsCard 
+                  id="01" 
+                  title="Flexible Learning Schedule" 
+                  desc="Fit your coursework around your existing commitments and obligations" 
+                  url="https://www.goodemailcode.com/email-code/template" />
+
+                <BenefitsCard 
+                  id="01" 
+                  title="Flexible Learning Schedule" 
+                  desc="Fit your coursework around your existing commitments and obligations" 
+                  url="https://www.goodemailcode.com/email-code/template" />
+
+                <BenefitsCard 
+                  id="01" 
+                  title="Flexible Learning Schedule" 
+                  desc="Fit your coursework around your existing commitments and obligations" 
+                  url="https://www.goodemailcode.com/email-code/template" />
               </div>
             </div>
         </section>
