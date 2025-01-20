3d301eb9a1467e1f84c0b9a86eb09105267d0abd
JosephOlu <161716305+JosephOluOlofinte@users.noreply.github.com>
2025-01-20 07:43:21 +0100

    Finished our courses section

---
+++ src/components/block/CourseCard.tsx
@@ -0,0 +1,64 @@
+
+import React from 'react'
+import Button from './Button';
+
+interface CourseCardProps {
+    image: string;
+    imageLink?: string;
+    imageAlt: string;
+    duration: string;
+    durationLink?: string;
+    level: string;
+    levelLink?: string;
+    author: string;
+    authorLink?: string;
+    title: string;
+    desc: string;
+    courselink: string;
+}
+
+const CourseCard: React.FC<CourseCardProps> = ({
+    image,
+    imageLink,
+    imageAlt,
+    duration,
+    durationLink,
+    level,
+    levelLink,
+    author,
+    authorLink,
+    title,
+    desc,
+    courselink,
+}) => {
+  return (
+                <div className="bg-absoluteWhite p-[24px] laptop:p-[30px] desktop:p-[40px] grid gap-[30px] xltablet:gap-[40px] desktop:gap-[35px] rounded-[10px]">
+
+                    <div className='grid gap-[20px]'>
+                        <a href={imageLink}><img src={image} alt={imageAlt} /></a>
+
+                        <div className="w-[100%] flex items-start flex-col gap-[16px] tablet:flex-row tablet:items-center xltablet:flex-col xltablet:items-start laptop:flex-row laptop:items-center tablet:justify-between ">
+                            <div className="w-[100%] tablet:w-auto flex justify-between tablet:justify-normal tablet:gap-[12px] xltablet:w-[100%] xltablet:justify-between laptop:justify-normal laptop:w-auto ">
+                                <a href={durationLink}><Button text={duration} variant="courseBtn" /></a>
+                                <a href={levelLink}><Button text={level} variant="courseBtn" /></a>
+                            </div>
+                            <a href={authorLink}>
+                            <h4 className="pl-[5px] laptop:pl-[0] font-medium text-[16px] desktop:text-[18px]">By {author}</h4>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className="grid gap-[14px] xltablet:gap-[16px] desktop:gap-[20px]">
+                      <h3 className="text-[20px] desktop:text-[22px] font-semibold text-[#333333]"> {title} </h3>
+                      <p className="text-grey30 xlmobile:text-[15px] desktop:text-[17px] leading-[1.5] "> {desc} </p>
+                    </div>
+
+                    <div className="flex justify-end">
+                      <a href={courselink} className="bg-white97 p-[14px] desktop:p-[20px] border-[2px] border-white95 rounded-[6px] w-[100%] text-center "> Get It Now </a>
+                    </div>
+                    
+                  </div>
+  )
+}
+
+export default CourseCard
--- src/pages/home/OurCourses.tsx
+++ src/pages/home/OurCourses.tsx
@@ -2,8 +2,13 @@
 
 import { Fragment } from "react/jsx-runtime"
 import Button from "../../components/block/Button"
-import BenefitsCard from "../../components/block/BenefitsCard"
-import UIUX from "../../assets/img/UIUXCourse.png"
+import CourseCard from "../../components/block/CourseCard"
+import webDev from "../../assets/img/webDev.png"
+import UIUX from "../../assets/img/UIUX.png"
+import mobileApp from "../../assets/img/mobile-app.png"
+import graphics from "../../assets/img/graphic-design.png"
+import frontend from "../../assets/img/frontend-dev.png"
+import advJavascript from "../../assets/img/advanced-javascript.png"
 
 
 
@@ -22,60 +27,89 @@ const OurCourses = () => {
 
               <div className="grid grid-cols-1 xltablet:grid-cols-2 grid-rows-6 tablet:grid-rows-3 gap-[24px] mt-[60px]">
 
-                  <div className="bg-absoluteWhite p-[24px] xltablet:p-[35px] desktop:p-[45px] grid gap-[30px] xltablet:gap-[40px] desktop:gap-[50px] rounded-[10px]">
+                  <CourseCard
+                  image={webDev}
+                  imageLink=""
+                  imageAlt="Web Design Fundamentals"
+                  duration="4 Weeks"
+                  durationLink=""
+                  level="Beginner"
+                  levelLink=""
+                  author="John Smith"
+                  authorLink=""
+                  title="Web Design Fundamentals"
+                  desc="Learn the fundamentals of web design, including HTML, CSS, and responsive design principles. Develop the skills to create visually appealing and user-friendly websites."
+                  courselink="" />
 
-                    <a href=""><img src={UIUX} alt="UI/UX course" /></a>
+                  <CourseCard
+                  image={UIUX}
+                  imageLink=""
+                  imageAlt="UI/UX Design"
+                  duration="6 Weeks"
+                  durationLink=""
+                  level="Intermediate"
+                  levelLink=""
+                  author="Emily Johnson"
+                  authorLink=""
+                  title="UI/UX Design"
+                  desc="Master the art of creating intuitive user interfaces (UI) and enhancing user experiences (UX). Learn design principles, wireframing, prototyping, and usability testing techniques."
+                  courselink="" />
 
-                    <div className="w-[100%] flex flex-col gap-[16px] tablet:flex-row tablet:items-center tablet:justify-between ">
-                      <div className="flex gap-[12px]">
-                        <a href=""><Button text="4 Weeks" variant="courseBtn" /></a>
-                        <a href=""><Button text="Beginner" variant="courseBtn" /></a>
-                      </div>
-                      <a href="">
-                      <h4 className="pl-[5px] tablet:pl-[0] font-medium text-[16px] desktop:text-[20px]">By John Smith</h4>
-                      </a>
-                    </div>
+                  <CourseCard
+                  image={mobileApp}
+                  imageLink=""
+                  imageAlt="Mobile App Development"
+                  duration="8 Weeks"
+                  durationLink=""
+                  level="Intermediate"
+                  levelLink=""
+                  author="David Brown"
+                  authorLink=""
+                  title="Mobile App Development"
+                  desc="Dive into the world of mobile app development. Learn to build native iOS and Android applications using industry-leading frameworks like Swift and Kotlin."
+                  courselink="" />
 
-                    <div className="grid gap-[14px] xltablet:gap-[16px] desktop:gap-[20px]">
-                      <h3 className="text-[20px] desktop:text-[22px] font-semibold text-[#333333]"> Web Design Fundamentals </h3>
-                      <p className="text-grey30 xlmobile:text-[15px] desktop:text-[17px] leading-[1.5] "> Learn the fundamentals of web design, including HTML, CSS, and responsive design principles. Develop the skills to create visually appealing and user-friendly websites. </p>
-                    </div>
+                  <CourseCard
+                  image={graphics}
+                  imageLink=""
+                  imageAlt="Graphic Design for Beginners"
+                  duration="10 Weeks"
+                  durationLink=""
+                  level="Beginner"
+                  levelLink=""
+                  author="Sarah Thompson"
+                  authorLink=""
+                  title="Graphic Design for Beginners"
+                  desc="Discover the fundamentals of graphic design, including typography, color theory, layout design, and image manipulation techniques. Create visually stunning designs for print and digital media."
+                  courselink="" />
 
-                    <div className="flex justify-end">
-                      <a href="" className="bg-white97 p-[14px] desktop:p-[20px] border-[2px] border-white95 rounded-[6px] w-[100%] text-center "> Get It Now </a>
-                    </div>
-                    
-                  </div>
+                  <CourseCard
+                  image={frontend}
+                  imageLink=""
+                  imageAlt="Front-End Web Development"
+                  duration="10 Weeks"
+                  durationLink=""
+                  level="Intermediate"
+                  levelLink=""
+                  author="Michael Adams"
+                  authorLink=""
+                  title="Front-End Web Development"
+                  desc="Become proficient in front-end web development. Learn HTML, CSS, JavaScript, and popular frameworks like Bootstrap and React. Build interactive and responsive websites."
+                  courselink="" />
 
-                <BenefitsCard 
-                  id="02" 
-                  title="Expert Instruction" 
-                  desc="Learn from industry experts who have hands-on experience in design and development." 
-                  url="https://www.goodemailcode.com/email-code/template" />
-
-                <BenefitsCard 
-                  id="03" 
-                  title="Diverse Course Offerings" 
-                  desc="Explore a wide range of design and development courses covering various topics." 
-                  url="https://www.goodemailcode.com/email-code/template" />
-
-                <BenefitsCard 
-                  id="04" 
-                  title="Updated Curriculum" 
-                  desc="Access courses with up-to-date content reflecting the latest trends and industry practices." 
-                  url="https://www.goodemailcode.com/email-code/template" />
-
-                <BenefitsCard 
-                  id="05" 
-                  title="Practical Projects and Assignments" 
-                  desc="Develop a portfolio showcasing your skills and abilities to potential employers." 
-                  url="https://www.goodemailcode.com/email-code/template" />
-
-                <BenefitsCard 
-                  id="06" 
-                  title="Interactive Learning Environment" 
-                  desc="Collaborate with fellow learners, exchanging ideas and feedback to enhance your understanding." 
-                  url="https://www.goodemailcode.com/email-code/template" />
+                  <CourseCard
+                  image={advJavascript}
+                  imageLink=""
+                  imageAlt="Advanced JavaScript"
+                  duration="6 Weeks"
+                  durationLink=""
+                  level="Advanced"
+                  levelLink=""
+                  author="Jennifer Wilson"
+                  authorLink=""
+                  title="Advanced JavaScript"
+                  desc="Take your JavaScript skills to the next level. Explore advanced concepts like closures, prototypes, asynchronous programming, and ES6 features. Build complex applications with confidence."
+                  courselink="" />
               </div>
             </div>
         </section>
