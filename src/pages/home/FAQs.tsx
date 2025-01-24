6d5cebd8910c146fd4fbd79e3496594cc914143a
JosephOlu <161716305+JosephOluOlofinte@users.noreply.github.com>
2025-01-24 17:36:23 +0100

    Finished FAQs layout

--- src/pages/home/FAQs.tsx
+++ src/pages/home/FAQs.tsx
@@ -2,6 +2,7 @@
 
 import { Fragment } from "react/jsx-runtime"
 import { Button } from "../../components"
+import Accordion from "../../components/block/Accordion"
 
 
 
@@ -9,13 +10,22 @@ const FAQs = () => {
   return (
     <Fragment>
         <section className="min-h-[40vh] w-[100%] flex justify-center mt-[70px]">
-            <div className="h-[100%] w-[90%] desktop:w-[80%] bg-absoluteWhite p-[24px] xltablet:p-[30px] lglaptop:p-[50px] grid grid-cols-1 gap-[50px]">
-              <div className="grid gap-[10px]">
-                <h2 className="font-semibold text-grey15">Frequently Asked Questions</h2>
-                <div className="grid xltablet:flex xltablet:justify-between xltablet:items-end">
-                  <p className="xltablet:w-[55ch] laptop:w-[70ch] lglaptop:w-[85ch] text-grey35 mt-[5px] pr-[5px] whitespace-break-spaces">Still you have any questions? Contact our Team via support@skillbridge.com</p>
-                  <div className="mt-5 xltablet:m-0"><Button text="See All FAQs" variant="whiteBtn" /></div>
+            <div className="h-[100%] w-[90%] desktop:w-[80%] bg-absoluteWhite p-[24px] xltablet:p-[30px] lglaptop:p-[50px] grid grid-cols-1 xltablet:grid-cols-2 gap-[50px]">
+              <div className="grid gap-[30px]">
+                <div className="grid gap-[15px]">
+                  <h2 className="font-semibold text-grey15 laptop:w-[15ch]">Frequently Asked Questions</h2>
+                  <p className="laptop:w-[45ch] text-grey35  pr-[5px] whitespace-break-spaces">
+                    Still you have any questions? Contact our Team via support@skillbridge.com
+                  </p>
                 </div>
+
+                  <Button text="See All FAQs" variant="whiteBtn" size="large" className="border-[1px]" />
+              </div>
+
+
+              <div className="border-[1px] rounded-2xl grid gap-[20px]">
+                <Accordion title="Accordion 1" description="Accordion 1" />
+
               </div>
 
 
