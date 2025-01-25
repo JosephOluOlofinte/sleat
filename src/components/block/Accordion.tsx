a59d45015636419d3186f7b7f7aa590e89b1c77e
JosephOlu <161716305+JosephOluOlofinte@users.noreply.github.com>
2025-01-25 21:18:13 +0100

    Update

--- src/components/block/Accordion.tsx
+++ src/components/block/Accordion.tsx
@@ -13,26 +13,37 @@ const Accordion: React.FC<AccordionProps> = ({ title, description }) => {
   };
 
   return (
-    <div style={{ margin: '10px 0', border: '1px solid #ccc', borderRadius: '5px' }}>
+    <div className='h-max' style={{ }}>
       <button
         onClick={toggleAccordion}
         style={{
           width: '100%',
           textAlign: 'left',
-          padding: '10px',
+          paddingBlock: isOpen ? '40px' : '24px',
+          paddingInline: '40px',
           cursor: 'pointer',
-          background: isOpen ? '#f0f0f0' : '#fff',
-          border: 'none',
+          background: '#24e',
+          border: isOpen ? '1px solid #F1F1F3' : 'none',
           outline: 'none',
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center'
         }}
       >
-        {title}
+        <h3 className='text-[16px] laptop:text-[18px] desktop:text-[20px] font-medium'>{title}</h3> 
+        { isOpen ? (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-10 p-[9px] bg-orange95 rounded-md">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+        </svg>
+        ) : (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="size-10 p-[9px] bg-orange95 rounded-md">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+      </svg>)}
+
       </button>
-      {isOpen && (
+      {isOpen ?
         <div style={{ padding: '10px', borderTop: '1px solid #ccc' }}>
           {description}
-        </div>
-      )}
+        </div> : null
+      }
     </div>
   );
 };
