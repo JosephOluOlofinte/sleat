96a6f601b3a5f822c6ffb0e14938b865c7da5f30
JosephOlu <161716305+JosephOluOlofinte@users.noreply.github.com>
2025-01-27 06:59:58 +0100

    Update. everything works fine

--- src/components/block/Accordion.tsx
+++ src/components/block/Accordion.tsx
@@ -39,12 +39,20 @@ const Accordion: React.FC<AccordionProps> = ({ title, description }) => {
       </button>
       {isOpen ?
       <>
-        <div className='border-t-[1px] border-white95 py-[20px] mt-[24px]'>
+        <div className='border-t-[1px] border-white95 py-[20px] laptop:py-[40px] desktop:py-[50px] mt-[24px]'>
           {description}
 
         </div>
 
-        <Button variant="greyBtn" size="large" text="Enrollment Process for Different Courses" />
+        <Button variant="greyBtn" size="large"> 
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-10 laptop:size-12 desktop:size-14">
+          <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm4.28 10.28a.75.75 0 0 0 0-1.06l-3-3a.75.75 0 1 0-1.06 1.06l1.72 1.72H8.25a.75.75 0 0 0 0 1.5h5.69l-1.72 1.72a.75.75 0 1 0 1.06 1.06l3-3Z" clipRule="evenodd" />
+        </svg>
+
+          {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10 laptop:size-12 desktop:size-14">
+            <path strokeLinecap="round" strokeLinejoin="round" d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+          </svg> */}
+        </Button>
         </>
         : null
       }
--- src/pages/home/FAQs.tsx
+++ src/pages/home/FAQs.tsx
@@ -11,11 +11,13 @@ const FAQs = () => {
     <Fragment>
         <section className="min-h-[40vh] w-[100%] flex justify-center mt-[70px]">
             <div className="
-            h-[100%] w-[90%] desktop:w-[80%] bg-absoluteWhite 
+          bg-absoluteWhite  rounded-xl
+            h-[100%] w-[90%] desktop:w-[80%]
             p-[24px] xltablet:p-[50px] laptop:p-[70px] lglaptop:p-[90px] desktop:p-[110px] 
-            grid grid-cols-1 laptop:grid-cols-3 
-            gap-[40px] laptop:gap-[70px] lglaptop:gap-[100px]">
-              <div className="grid gap-[30px]">
+            grid grid-cols-1 laptop:grid-cols-5 
+            gap-[40px] laptop:gap-[70px] lglaptop:gap-[100px]
+            ">
+              <div className="col-span-2 grid gap-[30px] h-max">
                 <div className="grid gap-[15px]">
                   <h2 className="font-semibold text-grey15 laptop:w-[15ch]">Frequently Asked Questions</h2>
                   <p className="laptop:w-[45ch] text-grey35  pr-[5px] whitespace-break-spaces">
@@ -26,7 +28,7 @@ const FAQs = () => {
                   <Button text="See All FAQs" variant="whiteBtn" size="large" className="border-[1px]" />
               </div>
 
-              <div className="rounded-2xl grid gap-[20px] h-max xltablet:min-w-[70%] laptop:col-span-2">
+              <div className="rounded-2xl grid gap-[20px] h-max xltablet:min-w-[70%] laptop:col-span-3">
                 <Accordion title="Accordion 1" description="Accordion 1" />
 
               </div>
