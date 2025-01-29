23133eb853f05fa6744e75f504d5a1c76c6fae14
JosephOlu <161716305+JosephOluOlofinte@users.noreply.github.com>
2025-01-29 23:48:43 +0100

    Update

--- src/components/static/Footer.tsx
+++ src/components/static/Footer.tsx
@@ -9,9 +9,9 @@ const Footer = () => {
       <footer className="bg-absoluteWhite w-[100%] flex justify-center mt-[70px]">
       <div className="h-[100%] w-[90%] desktop:w-[80%] rounded-[10px] pt-[50px] xltablet:pt-[80px] lglaptop:pt-[100px] pb-[30px] xltablet:p-[30px] lglaptop:p-[50px] grid grid-cols-1 xltablet:grid-cols-2 gap-6">
 
-        <div className="w-[44px] xltablet:w-[48px] lglaptop:w-[54px]">
+        <div className="w-[44px] xltablet:w-[48px] lglaptop:w-[54px] grid h-max gap-7">
           <div><img src={Logo} alt="" /></div>
-          <ul>
+          <ul className="grid h-max gap-2">
             <li className="flex gap-2 items-center w-max">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                 <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
@@ -21,10 +21,10 @@ const Footer = () => {
             </li>
 
             <li className="flex gap-2 items-center w-max">
-              <div><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+              <div><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
                 <path fillRule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clipRule="evenodd" />
               </svg></div>
-              <p>+123 456 7890</p>
+              <p>+91 91813 23 2309</p>
             </li>
 
             <li className="flex gap-2 items-center w-max">
