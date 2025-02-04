151df6f2173fc03cd1c889d16dc212d026d7c321
JosephOlu <161716305+JosephOluOlofinte@users.noreply.github.com>
2025-02-04 06:35:38 +0100

    update

--- src/components/block/Button.tsx
+++ src/components/block/Button.tsx
@@ -40,7 +40,7 @@ const Button: React.FC<ButtonProps> = ({
         secondary: "bg-absoluteWhite border-[1px] border-absoluteWhite hover:text-orange50 hover:border-orange50 ease-in-out duration-500",
         danger: "bg-red-500 text-absoluteWhite hover:bg-red-600 focus:ring-red-300",
         whiteBtn: "bg-absoluteWhite hover:bg-white95 ease-in-out duration-500 text-absoluteBlack text-[15px] text-grey15 font-medium inline-block",
-        greyBtn: "bg-white97 border-[1px] border-white95 rounded-[6px] w-[100%] text-center",
+        greyBtn: "bg-white97 border-[1px] border-white95 rounded-[6px] w-[100%] text-center hover:bg-white95 hover:border-white90 ease-in-out duration-500",
         courseBtn: "bg-absoluteWhite border-white95 hover:bg-white97 hover:border-white90 ease-in-out duration-500 border-[1px] text-grey30 text-[12px] xltablet:text-[14px] desktop:text-[16px]",
     }
 
--- src/pages/courseslist/WebDesign.tsx
+++ src/pages/courseslist/WebDesign.tsx
@@ -10,14 +10,33 @@ const WebDesign = () => {
         <section className="flex justify-center">
             <div className="
             bg-absoluteWhite 
-            min-h-[10vh] w-[90%] desktop:w-[80%]">
+            min-h-[10vh] w-[90%] desktop:w-[80%]
+            p-[24px] laptop:p-[30px] desktop:p-[40px] 
+            grid gap-[30px] xltablet:gap-[40px] desktop:gap-[35px] rounded-[10px]
+            ">
+
               <div className="grid gap-[10px]">
-                <h2 className="font-semibold text-grey15">Our Courses</h2>
+                <h2 className="font-semibold text-grey15 text-[18px] xltablet:text-[20px] lglaptop:text-[24px]">Web Design Fundamentals</h2>
                 <div className="grid xltablet:flex xltablet:justify-between xltablet:items-end">
                   <p className="xltablet:w-[55ch] laptop:w-[70ch] lglaptop:w-[85ch] text-grey35 mt-[5px] pr-[5px] whitespace-break-spaces">Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam eget elit id imperdiet et. Cras eu sit dignissim lorem nibh et. Ac cum eget habitasse in velit fringilla feugiat senectus in.</p>
-                  <div className="mt-5 xltablet:m-0"><Button text="View All" variant="whiteBtn" /></div>
+                  <div className="mt-5 xltablet:m-0"><Button text="View Course" variant="courseBtn" size="small" /></div>
                 </div>
               </div>
+
+              {/* IMAGES, DURATION, LEVEL, AND AUTHOR */}
+              <div className='grid gap-[20px]'>
+                <a href=""><img src="" alt="" /></a>
+
+                <div className="w-[100%] flex items-start flex-col gap-[16px] tablet:flex-row tablet:items-center xltablet:flex-col xltablet:items-start laptop:flex-row laptop:items-center tablet:justify-between ">
+                    <div className="w-[100%] tablet:w-auto flex justify-between tablet:justify-normal tablet:gap-[12px] xltablet:w-[100%] xltablet:justify-between laptop:justify-normal laptop:w-auto ">
+                        <Button text="4 Weeks" variant="courseBtn" size="small" />
+                        <Button text="Beginner" variant="courseBtn" size="small" url="" />
+                    </div>
+                    <a href="">
+                    <p className="pl-[5px] laptop:pl-[0] font-medium text-[16px] desktop:text-[18px]">By John Smith</p>
+                    </a>
+                </div>
+            </div>
             </div>
         </section>
     </Fragment>
